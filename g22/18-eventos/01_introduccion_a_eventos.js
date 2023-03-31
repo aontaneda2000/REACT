398cfcba0c72a35f00ebf3304e91fcd557ba6337
Aaron <aron_ch37@outlook.es>
2023-03-31 06:29:04 -0500

    add directorys

--- g22/18-eventos/01_introduccion_a_eventos.js
+++ g22/18-eventos/01_introduccion_a_eventos.js
@@ -12,7 +12,7 @@
 // => 1) Atributo HTML. Un handler puede ser establecido en el HTML con un atributo llamado on<event>.
 
 function showMessage() {
-  window.alert('Saludando desde atributo HTML')
+  window.alert("Saludando desde atributo HTML");
 }
 
 // => 2) Propiedad del DOM. Podemos asignar un handler usando una propiedad del DOM on<event>.
@@ -23,15 +23,15 @@ function showMessage() {
 
 // FORMA 1
 function handler() {
-  window.alert('Saludando desde pripiedad del DOM')
+  window.alert("Saludando desde pripiedad del DOM");
 }
 
-prop.onclick = handler
+prop.onclick = handler;
 
 // FOMRA 2
 prop.onclick = function () {
-  window.alert('Saludando desde pripiedad del DOM')
-}
+  window.alert("Saludando desde pripiedad del DOM");
+};
 
 // => 3) Métodos. Podemos usar el método addEventListener para agregar un handler a un elemento.
 /* La sintaxis para agregar un handler:
@@ -52,13 +52,15 @@ prop.onclick = function () {
 
 // forma 2
 
-content.addEventListener('click', (event) => {
+content.addEventListener("click", (event) => {
   // Objeto del Evento. Cuando un evento ocurre, el navegador crea un objeto del evento que coloca los detalles dentro y los pasa como un argumento al handler.
 
   // console.log(event.type)
   // console.log(event)
 
   // => Elemento objetivo (target). El elemento que realmente inició el evento.
-  console.log('El que originó el evento', event.target)
-  console.log('El elemento que tiene el escuchador', event.currentTarget)
-})
+  console.log("El que originó el evento", event.target);
+
+  // remplaza el this en los arrow functions
+  console.log("El elemento que tiene el escuchador", event.currentTarget);
+});
